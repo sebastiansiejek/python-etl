d232fade5bc9c367435356771fd00be6523adc0a
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-04-18 21:04:23 +0200

    feat: measure time of select

--- main.py
+++ main.py
@@ -25,10 +25,16 @@ if __name__ == "__main__":
         end = timer()
         print(f"Import listening sessions: {round(end - start, 2)}s")
 
+    start = timer()
     the_most_popular_artist = get_the_most_popular_artist()
+    end = timer()
     print("Artysta z największą ilością odtworzeń\n", the_most_popular_artist.Artist, ", ", the_most_popular_artist.Count)
+    print(f"Time: {round(end - start, 2)}s")
 
+    start = timer()
     the_most_popular_tracks = get_the_most_popular_tracks()
     print("Najbardziej pupularne utwory")
     for track in the_most_popular_tracks:
         print(track.track_title.replace("\n", ""), track.Count)
+    end = timer()
+    print(f"Time: {round(end - start, 2)}s")
