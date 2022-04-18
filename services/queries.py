a8e7e9e53f7176f8602c36e857e09a686771b1e1
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-04-18 21:03:42 +0200

    style: move second select arg to next line

--- services/queries.py
+++ services/queries.py
@@ -17,13 +17,15 @@ def get_the_most_popular_artist():
     # ORDER BY
     #     "Liczba odsłuchań" DESC
     # LIMIT 1
-    query = (TracksEntity.select(TracksEntity.artist_name.alias("Artist"), peewee.fn.COUNT(TracksEntity.artist_name).alias("Count"))
+    query = (TracksEntity.select(TracksEntity.artist_name.alias("Artist"),
+                                 peewee.fn.COUNT(TracksEntity.artist_name).alias("Count"))
              .join(ListeningSessionsEntity, on=(ListeningSessionsEntity.track_id == TracksEntity.track_id))
              .group_by(TracksEntity.artist_name)
              .order_by(peewee.fn.COUNT(TracksEntity.artist_name).desc())
              .limit(1))
 
     return query[0]
+
 
 def get_the_most_popular_tracks():
     query = (TracksEntity.select(TracksEntity.track_title, peewee.fn.COUNT(TracksEntity.track_title).alias("Count"))
