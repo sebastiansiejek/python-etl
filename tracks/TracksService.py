5977462653e867d22b115b4da5dbbd03abd89e74
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-04-20 18:00:29 +0200

    feat: add imported info

--- tracks/TracksService.py
+++ tracks/TracksService.py
@@ -3,3 +3,4 @@ from tracks.TracksEntity import TracksEntity
 
 def add_from_list_to_tracks(row):
     TracksEntity.create(performance_id=row[0], track_id=row[1], artist_name=row[2], track_title=row[3])
+    print(f'imported: {row}')
