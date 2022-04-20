d8595b98f62df066e1b92539473c52b49b8f01df
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-04-20 17:59:44 +0200

    feat: add imported info

--- services/PrepareTxtFileToDatabase.py
+++ services/PrepareTxtFileToDatabase.py
@@ -1,6 +1,7 @@
 class PrepareTxtFileToDatabase:
     def __init__(self, path):
         self.path = path
+
     def prepare(self, callback=()):
         with open(self.path, mode="r", encoding="ISO-8859-1") as file:
             for item in file:
